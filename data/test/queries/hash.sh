3788ddf151af2c6654de1ada8b6e5bf9b7363391
Christoph Haag <haagch.christoph@googlemail.com>
2011-07-04 17:33:08 +0200

    usage information added

--- data/test/queries/hash.sh
+++ data/test/queries/hash.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 #based on http://broadcast.oreilly.com/2009/12/principles-for-standardized-rest-authentication.html
 
+if [ $# -ne 2 ]
+then
+   echo "Usage: hash.sh <file: jsonrequest> <secret>"
+   exit 0
+fi
+
+
 BODYFILE="$1"
 SECRET="$2"
 BODYHASH=$(sha1sum "$BODYFILE" | egrep -o -e '[0123456789abcdef]*')
