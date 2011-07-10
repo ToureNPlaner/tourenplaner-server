bb390350a801caa9a24722c4f2e8607a55780bca
Peter Vollmer <vollmerpeter@googlemail.com>
2011-07-10 15:23:22 +0200

    modified kstest and sptest for multiple servers

--- data/test/queries/sptest.sh
+++ data/test/queries/sptest.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-./curl.sh komani.ath.cx:8081 simpleSP.json sp FooUser FooPassword
+
+if [ $# -ne 1 ] 
+then
+   echo "Usage: hash.sh <URL> "
+   echo "Example: #/curl.sh komani.ath.cx:8081"
+   exit 1
+fi
+
+./curl.sh $1 simpleSP.json sp FooUser FooPassword
