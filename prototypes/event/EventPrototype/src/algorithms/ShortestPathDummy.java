3e7bbdc28202c6505b493f5a7c3f7e3978fe5536
Niklas Schnelle <niklas@komani.de>
2011-07-27 13:48:35 +0200

    Respond with sane path

--- prototypes/event/EventPrototype/src/algorithms/ShortestPathDummy.java
+++ prototypes/event/EventPrototype/src/algorithms/ShortestPathDummy.java
@@ -26,30 +26,95 @@ public class ShortestPathDummy implements Algorithm {
 		
 		List<Float> temp;
 		route = new ArrayList<List<Float>>(5);
-		
+
 		temp = new ArrayList<Float>(2);
-		temp.add(0, 3.1427F);
-		temp.add(1, 90.1487F);
+		temp.add(0, 48.781773F);
+		temp.add(1, 9.175818F);
 		route.add(temp);
 		
 		temp = new ArrayList<Float>(2);
-		temp.add(0, 28.1427F);
-		temp.add(1, 20.1567F);
+		temp.add(0, 48.782043F);
+		temp.add(1, 9.175794F);
 		route.add(temp);
 		
 		temp = new ArrayList<Float>(2);
-		temp.add(0, 17.1978F);
-		temp.add(1, 86.1487F);
+		temp.add(0, 48.782295F);
+		temp.add(1, 9.175640F);
 		route.add(temp);
 		
 		temp = new ArrayList<Float>(2);
-		temp.add(0, 42.5927F);
-		temp.add(1, 129.4667F);
+		temp.add(0, 48.782352F);
+		temp.add(1, 9.175596F);
 		route.add(temp);
 		
 		temp = new ArrayList<Float>(2);
-		temp.add(0, 89.1427F);
-		temp.add(1, 1.0847F);
+		temp.add(0, 48.783245F);
+		temp.add(1, 9.174920F);
+		route.add(temp);
+		
+		temp = new ArrayList<Float>(2);
+		temp.add(0, 48.783611F);
+		temp.add(1, 9.176027F);
+		route.add(temp);
+		
+		temp = new ArrayList<Float>(2);
+		temp.add(0, 48.784061F);
+		temp.add(1, 9.177478F);
+		route.add(temp);
+		
+		temp = new ArrayList<Float>(2);
+		temp.add(0, 48.784279F);
+		temp.add(1, 9.178200F);
+		route.add(temp);
+		
+		temp = new ArrayList<Float>(2);
+		temp.add(0, 48.784256F);
+		temp.add(1, 9.178476F);
+		route.add(temp);
+		
+		temp = new ArrayList<Float>(2);
+		temp.add(0, 48.784222F);
+		temp.add(1, 9.178576F);
+		route.add(temp);
+		
+		temp = new ArrayList<Float>(2);
+		temp.add(0, 48.784187F);
+		temp.add(1,  9.178683F);
+		route.add(temp);
+		
+		temp = new ArrayList<Float>(2);
+		temp.add(0, 48.784172F);
+		temp.add(1, 9.178791F);
+		route.add(temp);
+		
+		temp = new ArrayList<Float>(2);
+		temp.add(0, 48.784172F);
+		temp.add(1, 9.178791F);
+		route.add(temp);
+
+		temp = new ArrayList<Float>(2);
+		temp.add(0, 48.784168F);
+		temp.add(1,  9.178823F);
+		route.add(temp);
+
+		temp = new ArrayList<Float>(2);
+		temp.add(0, 48.784168F);
+		temp.add(1, 9.178838F);
+		route.add(temp);
+
+		temp = new ArrayList<Float>(2);
+		temp.add(0, 48.784142F);
+		temp.add(1, 9.178848F);
+		route.add(temp);
+
+		temp = new ArrayList<Float>(2);
+		temp.add(0, 48.784107F);
+		temp.add(1, 9.178863F);
+		route.add(temp);
+
+		temp = new ArrayList<Float>(2);
+		temp.add(0, 48.784061F);
+		temp.add(1, 9.178906F);
 		route.add(temp);
 		
 	}
