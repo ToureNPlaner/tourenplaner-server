d81cf4359f7b6c083102b3603e3f6bba4ccabee4
Peter Vollmer <vollmerpeter@googlemail.com>
2011-09-15 15:05:30 +0200

    add grafrepfactory and graphreptextreader edit all references

--- src/graphrep/GraphRep.java
+++ src/graphrep/GraphRep.java
@@ -1,32 +1,18 @@
 package graphrep;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Serializable;
-
 /**
  * Class GraphRep
  */
-public class GraphRep implements Serializable {
-	private static final long serialVersionUID = 1L;
+public class GraphRep {
 
-	private final NNSearcher searcher;
+	protected NNSearcher searcher;
 
-	private final int nodeCount;
-	private final int edgeCount;
+	protected int nodeCount;
+	protected int edgeCount;
 
 	// note: edgeNum often refers to the relative number of an outgoing edge
 	// from a node:
 	// (first outgoing edge = 0, second outgoing edge = 1, ...)
-
-	// (deprecated) File Format:
-	// TODO: Ask Frederic for updated description (#7)
-	// #N -> Number of Nodes
-	// #X -> Number of Edges:
-	// #N * (ID oldOsmID lat lon height)
-	// #M * (source destination multiplier distance elevationDelta)
 
 	// new fileformat is probably:
 	// nodecount
@@ -50,163 +36,33 @@ public class GraphRep implements Serializable {
 	// nodes
 	// why no osm id??
 	// private final int[] osmIDs;
-	protected final float[] lat;
-	protected final float[] lon;
-	private final int[] height;
+	protected float[] lat;
+	protected float[] lon;
+	protected int[] height;
 
 	// edges
-	private final int[] source_out;
-	private final int[] dest_out;
-	private final float[] mult_out;
-	private final int[] dist_out;
-	// private final float[] elev;
+	protected int[] source_out;
+	protected int[] dest_out;
+	protected float[] mult_out;
+	protected int[] dist_out;
+	protected float[] elev_out;
 
-	final int[] offsetOut;
+	protected int[] offsetOut;
 
-	private final int[] source_in;
-	private final int[] dest_in;
-	private final float[] mult_in;
-	private final int[] dist_in;
+	protected int[] source_in;
+	protected int[] dest_in;
+	protected float[] mult_in;
+	protected int[] dist_in;
+	protected float[] elev_in;
 
-	final int[] offsetIn;
+	protected int[] offsetIn;
 
 	/**
-	 * Constructor loads graph from disk
 	 * 
-	 * @throws IOException
 	 * 
 	 */
-	public GraphRep(String filename) throws IOException {
+	public GraphRep() {
 
-		BufferedReader in = null;
-		try {
-			in = new BufferedReader(new FileReader(filename));
-
-		} catch (FileNotFoundException e) {
-			// TODO: what happens here?
-			e.printStackTrace();
-		}
-
-		String line;
-
-		// exception should happen when file format is wrong
-		line = in.readLine();
-
-		while (line != null && line.trim().startsWith("#")) {
-			line = in.readLine();
-		}
-		if (line != null) {
-			nodeCount = Integer.parseInt(line);
-		} else {
-			nodeCount = 0;
-		}
-
-		line = in.readLine();
-		if (line != null) {
-			edgeCount = Integer.parseInt(line);
-		} else {
-			edgeCount = 0;
-		}
-
-		// TODO osmIDs = new int[nodeCount];
-		lat = new float[nodeCount];
-		lon = new float[nodeCount];
-		height = new int[nodeCount];
-
-		offsetOut = new int[nodeCount + 1];
-
-		source_out = new int[edgeCount];
-		dest_out = new int[edgeCount];
-		mult_out = new float[edgeCount];
-		// TODO elev = new float[edgeCount];
-		dist_out = new int[edgeCount];
-
-		// used for splitted lines in 1. nodes 2. edges
-		String[] splittedLine;
-		System.out.println("Reading " + nodeCount + " nodes and " + edgeCount
-				+ " edges...");
-		for (int i = 0; i < nodeCount; i++) {
-			splittedLine = in.readLine().split(" ");
-			// TODO osmIDs[i] = Integer.parseInt(linesplitted[1]);
-			lat[i] = Float.parseFloat(splittedLine[1]);
-			lon[i] = Float.parseFloat(splittedLine[2]);
-			height[i] = Integer.parseInt(splittedLine[3]);
-		}
-
-		System.out.println("successfully read nodes");
-		int currentSource;
-		int prevSource = -1;
-		for (int i = 0; i < edgeCount; i++) {
-			splittedLine = in.readLine().split(" ");
-			currentSource = Integer.parseInt(splittedLine[0]);
-			source_out[i] = currentSource;
-			dest_out[i] = Integer.parseInt(splittedLine[1]);
-			dist_out[i] = Integer.parseInt(splittedLine[2]);
-			mult_out[i] = Float.parseFloat(splittedLine[3]);
-			// TODO mult[i] = Integer.parseInt(splittedLine[4]);
-
-			if (currentSource != prevSource) {
-				for (int j = currentSource; j > prevSource; j--) {
-					offsetOut[j] = i;
-				}
-				prevSource = currentSource;
-			}
-		}
-		in.close();
-		offsetOut[nodeCount] = edgeCount;
-		// assuming we have at least one edge
-		for (int cnt = nodeCount - 1; offsetOut[cnt] == 0; cnt--) {
-			offsetOut[cnt] = offsetOut[cnt + 1];
-		}
-
-		System.out.println("successfully read outedges");
-
-		// //// inedges
-		filename = System.getProperty("user.home") + "/germany.txt_inedges.txt";
-		try {
-			in = new BufferedReader(new FileReader(filename));
-
-		} catch (FileNotFoundException e) {
-			// TODO: what happens here?
-			e.printStackTrace();
-		}
-		offsetIn = new int[nodeCount + 1];
-
-		source_in = new int[edgeCount];
-		dest_in = new int[edgeCount];
-		mult_in = new float[edgeCount];
-		// TODO elev = new float[edgeCount];
-		dist_in = new int[edgeCount];
-		// shamelessly reuse variables
-		int prevDest = -1;
-		int currentDest;
-		for (int i = 0; i < edgeCount; i++) {
-			splittedLine = in.readLine().split(" ");
-			currentDest = Integer.parseInt(splittedLine[1]);
-			source_in[i] = Integer.parseInt(splittedLine[0]);
-			dest_in[i] = currentDest;
-			dist_in[i] = Integer.parseInt(splittedLine[2]);
-			mult_in[i] = Float.parseFloat(splittedLine[3]);
-			// TODO mult[i] = Integer.parseInt(splittedLine[4]);
-			if (currentDest != prevDest) {
-				for (int j = currentDest; j > prevDest; j--) {
-					offsetIn[j] = i;
-				}
-				prevDest = currentDest;
-			}
-		}
-		in.close();
-		offsetIn[nodeCount] = edgeCount;
-		// assuming we have at least one edge
-		for (int cnt = nodeCount - 1; offsetIn[cnt] == 0; cnt--) {
-			offsetIn[cnt] = offsetIn[cnt + 1];
-		}
-		System.out.println("successfully read inedges");
-
-		// choose the NNSearcher here
-		// DumbNN uses linear search and is slow.
-		// KDTreeNN should be faster
-		searcher = new DumbNN(this);
 	}
 
 	/**
@@ -255,7 +111,7 @@ public class GraphRep implements Serializable {
 	 * @param nodeId
 	 */
 	public final int getOutEdgeCount(int nodeId) {
-		return (offsetOut[nodeId + 1] - offsetOut[nodeId]);
+		return offsetOut[nodeId + 1] - offsetOut[nodeId];
 	}
 
 	/**
--- src/utils/GraphSerializer.java
+++ src/utils/GraphSerializer.java
@@ -10,10 +10,10 @@ public class GraphSerializer {
 	public static void main(String[] args) {
 		GraphRep g = null;
 		try {
-			String filename = (args.length == 1 && args[0] != null) ? args[0]
+			String filename = args.length == 1 && args[0] != null ? args[0]
 					: System.getProperty("user.home")
 							+ "/serializedGraphrep.dat";
-			g = new GraphRep(System.getProperty("user.home")+"germany.txt");
+			g = new GraphRep();
 			FileOutputStream fout = new FileOutputStream(filename);
 			ObjectOutputStream oos = new ObjectOutputStream(fout);
 			oos.writeObject(g);
