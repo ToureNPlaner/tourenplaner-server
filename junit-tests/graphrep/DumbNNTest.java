17fc7e77a9a2355c66dd6aa45614f09220675bd2
Peter Vollmer <vollmerpeter@googlemail.com>
2011-09-20 14:02:34 +0200

    Add some junittests for graphrep package

---
+++ junit-tests/graphrep/DumbNNTest.java
@@ -0,0 +1,27 @@
+package graphrep;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DumbNNTest {
+
+	@Test
+	public final void testGetIDForCoordinates() {
+
+		GraphRep graphRep = new GraphRep();
+		graphRep.lat = new float[3];
+		graphRep.lon = new float[3];
+		graphRep.lat[0] = 2.5F;
+		graphRep.lat[1] = 5.0F;
+		graphRep.lat[2] = 2.0F;
+		graphRep.lon[0] = 1.5F;
+		graphRep.lon[1] = 3.0F;
+		graphRep.lon[2] = 4.0F;
+		graphRep.nodeCount = 3;
+
+		graphrep.DumbNN dumpNN = new DumbNN(graphRep);
+		assertEquals("getIDForCoordinates", 0,
+				dumpNN.getIDForCoordinates(1.5F, 2.0F));
+	}
+}
---
+++ junit-tests/graphrep/HeapTest.java
@@ -0,0 +1,44 @@
+package graphrep;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HeapTest {
+
+	@Test
+	public final void testHeap() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testInsert() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testIsEmpty() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testPeekMinId() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testPeekMinDist() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testRemoveMin() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testResetHeap() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
