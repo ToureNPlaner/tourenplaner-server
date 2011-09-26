32d4bdf18fafc479c200d0ededc1d319416f652d
Peter Vollmer <vollmerpeter@googlemail.com>
2011-09-26 10:30:30 +0200

    finished heap tests

--- junit-tests/graphrep/HeapTest.java
+++ junit-tests/graphrep/HeapTest.java
@@ -1,44 +1,81 @@
 package graphrep;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class HeapTest {
 
 	@Test
-	public final void testHeap() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
 	public final void testInsert() {
-		fail("Not yet implemented"); // TODO
+		Heap heap = new Heap();
+
+		heap.insert(23456, 65432);
+		heap.insert(12345, 54321);
+		heap.insert(34567, 76543);
+
+		assertEquals(12345, heap.peekMinId());
+		assertEquals(54321, heap.peekMinDist());
 	}
 
 	@Test
 	public final void testIsEmpty() {
-		fail("Not yet implemented"); // TODO
+		Heap heap = new Heap();
+		assertEquals(true, heap.isEmpty());
+		heap.insert(12345, 54321);
+		assertEquals(false, heap.isEmpty());
+		heap.removeMin();
+		assertEquals(true, heap.isEmpty());
 	}
 
 	@Test
 	public final void testPeekMinId() {
-		fail("Not yet implemented"); // TODO
+		Heap heap = new Heap();
+
+		heap.insert(23456, 65432);
+		heap.insert(12345, 54321);
+		heap.insert(34567, 76543);
+
+		assertEquals(12345, heap.peekMinId());
+
 	}
 
 	@Test
 	public final void testPeekMinDist() {
-		fail("Not yet implemented"); // TODO
+		Heap heap = new Heap();
+
+		heap.insert(23456, 65432);
+		heap.insert(12345, 54321);
+		heap.insert(34567, 76543);
+
+		assertEquals(54321, heap.peekMinDist());
 	}
 
 	@Test
 	public final void testRemoveMin() {
-		fail("Not yet implemented"); // TODO
+		Heap heap = new Heap();
+
+		heap.insert(23456, 65432);
+		heap.insert(12345, 54321);
+		heap.insert(34567, 76543);
+
+		heap.removeMin();
+		assertEquals(23456, heap.peekMinId());
+		assertEquals(65432, heap.peekMinDist());
+
 	}
 
 	@Test
 	public final void testResetHeap() {
-		fail("Not yet implemented"); // TODO
+		Heap heap = new Heap();
+
+		heap.insert(23456, 65432);
+		heap.insert(12345, 54321);
+		heap.insert(34567, 76543);
+
+		heap.resetHeap();
+
+		assertEquals(true, heap.isEmpty());
 	}
 
 }
