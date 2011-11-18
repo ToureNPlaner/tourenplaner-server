fb0e7fe32dc671de511b6562f7411d33ef29b211
Steffen Hanikel <steffen.hanikel@googlemail.com>
2011-11-18 14:39:37 +0100

    Enable HTTP-Content Compression

--- src/server/ServerPipelineFactory.java
+++ src/server/ServerPipelineFactory.java
@@ -11,6 +11,7 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.handler.codec.http.HttpChunkAggregator;
+import org.jboss.netty.handler.codec.http.HttpContentCompressor;
 import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
 import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
 
@@ -62,7 +63,7 @@ public class ServerPipelineFactory implements ChannelPipelineFactory {
 		pipeline.addLast("aggregator", new HttpChunkAggregator(10485760));
 		pipeline.addLast("encoder", new HttpResponseEncoder());
 		// We could add compression support by uncommenting the following line
-		// pipeline.addLast("deflater", new HttpContentCompressor());
+		pipeline.addLast("deflater", new HttpContentCompressor());
 
 		pipeline.addLast("handler", new HttpRequestHandler(mapper, cCore,
 				serverInfo));
