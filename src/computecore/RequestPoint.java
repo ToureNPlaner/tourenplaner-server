175bfa7f99a9048f880f2c45e7060dd14aa2200d
Niklas Schnelle <niklas@komani.de>
2011-12-26 17:06:38 +0100

    RequestPoints no longer extends Points but keeps Interface
    This will enable proper reordering

---
+++ src/computecore/RequestPoint.java
@@ -0,0 +1,44 @@
+package computecore;
+
+import java.util.Map;
+
+/**
+ * Author: Niklas Schnelle
+ * Date: 12/26/11
+ * Time: 4:46 PM
+ */
+public class RequestPoint {
+    private int lat;
+    private int lon;
+    private Map<String, Object> constraints;
+
+    public RequestPoint(int lat, int lon, Map<String, Object> constraints) {
+        this.lat = lat;
+        this.lon = lon;
+        this.constraints = constraints;
+    }
+
+    public int getLat() {
+        return lat;
+    }
+
+    public void setLat(int lat) {
+        this.lat = lat;
+    }
+
+    public int getLon() {
+        return lon;
+    }
+
+    public void setLon(int lon) {
+        this.lon = lon;
+    }
+
+    public Map<String, Object> getConstraints() {
+        return constraints;
+    }
+
+    public void setConstraints(Map<String, Object> constraints) {
+        this.constraints = constraints;
+    }
+}
