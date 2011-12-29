a79f1e6e8254ec6a53d27a7ce05f95370de47c42
Niklas Schnelle <niklas@komani.de>
2011-12-29 22:51:23 +0100

    TSP with greedy algorithm
    still need to fix that that start and target get connected but that
    will need changes to ShortestPathCH.java so that's for another commit.
    The greedy algorithm starts at the first "city" and chooses the nearest
    unvisited city as it's next move.

--- src/algorithms/TravelingSalesman.java
+++ src/algorithms/TravelingSalesman.java
@@ -1,11 +1,15 @@
 package algorithms;
 
 import com.carrotsearch.hppc.BitSet;
+import com.carrotsearch.hppc.IntOpenHashSet;
 import computecore.ComputeRequest;
+import computecore.RequestPoint;
 import computecore.RequestPoints;
 import graphrep.GraphRep;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -63,7 +67,40 @@ public class TravelingSalesman extends GraphAlgorithm {
             points[i] = graph.getIdForCoordinates(requestPoints.getPointLat(i), requestPoints.getPointLon(i));
         }
         try {
+            // Looks cheap but computes the n^2 matrix of distances for the given points
             distmat = computeDistMatrix(points);
+
+
+
+            IntOpenHashSet visited = new IntOpenHashSet(points.length);
+            int currIndex = 0;
+            int minValue;
+            int minIndex;
+            List<RequestPoint> requestPointList = req.getPoints().getStore();
+            List<RequestPoint> pointStore = new ArrayList<RequestPoint>(points.length);
+            // add the initial point
+            pointStore.add(requestPointList.get(currIndex));
+            
+            for (int nextIndex = 1; nextIndex < points.length; nextIndex++) {
+                visited.add(currIndex);
+
+                minValue = Integer.MAX_VALUE;
+                minIndex = 0;
+                for (int i = 0; i < points.length; i++) {
+                    if (minValue > distmat[currIndex][i] && i != currIndex && !visited.contains(i)) {
+                        minValue = distmat[currIndex][i];
+                        minIndex = i;
+                    }
+                }
+                currIndex = minIndex;
+                pointStore.add(requestPointList.get(currIndex));
+            }
+            
+            req.getPoints().setStore(pointStore);
+            // Now build real paths
+            chdijks.shortestPath(requestPoints, req.getResulWay());
+
+
             debugMat.put("distMat", distmat);
             req.setMisc(debugMat);
         } catch (IllegalAccessException e) {
