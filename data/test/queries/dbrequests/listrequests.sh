e19776e2d9dadac6b65d9e6c9abd0dca1b5edf82
Sascha Meusel <meuselsa@studi.informatik.uni-stuttgart.de>
2012-02-13 17:54:02 +0100

    Modified and added curl scripts for modified and new url handlers.

--- data/test/queries/dbrequests/listrequests.sh
+++ data/test/queries/dbrequests/listrequests.sh
@@ -3,7 +3,7 @@
 if [ $# -ne 3 ]
 then
    echo "Usage: $0 <PARAMETERS> <URL> <HTTP/HTTPS>"
-   echo "Example 1: #$0 \"?id=all&limit=5&offset=10&details=nojson\" komani.ath.cx:8081 HTTPS"
+   echo "Example 1: #$0 \"?id=all&limit=5&offset=10\" komani.ath.cx:8081 HTTPS"
    echo "Example 2: #$0 \"?id=36&limit=5&offset=10\" komani.ath.cx:8081 HTTPS"
    echo "Example 3: #$0 \"?limit=5&offset=10\" komani.ath.cx:8081 HTTPS"
    exit 1
