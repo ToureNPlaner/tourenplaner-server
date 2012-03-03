534d2da981a9e306b6627f9e0195746b5815796b
Niklas Schnelle <niklas@komani.de>
2012-03-03 14:44:35 +0100

    Save mapping and offset in binary graph so we don't need to run generateOffsets()

--- src/graphrep/GraphRepBinaryReader.java
+++ src/graphrep/GraphRepBinaryReader.java
@@ -1,84 +1,110 @@
 package graphrep;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.logging.Logger;
 
 public class GraphRepBinaryReader implements GraphRepReader {
     private static Logger log = Logger.getLogger("tourenplaner");
-    
-    private static final int version = 1;
 
-	// new fileformat is probably:
-	// nodecount
-	// edgecount
-	// (nodecount *) ID lat lon height
-	// (edgecount *) source trgt dist mult
+    private static final int version = 2;
 
-	@Override
-	public GraphRep createGraphRep(InputStream in) throws IOException {
+    // fileformat is:
+    // nodecount
+    // edgecount
+    // (nodecount) * (lat lon height rank)
+    // (edgecount) * (source trgt dist mult)
+    // (edgecount) * (mappingInToOut)
+    // (nodecount+1) * (offsetIn)
+    // (nodecount+1) * (offsetOut)
+
+    @Override
+    public GraphRep createGraphRep(InputStream in) throws IOException {
 
         // 8388608 Bytes = 8 MB
         BufferedInputStream bin = new BufferedInputStream(in, 8388608);
-        DataInputStream din  = new DataInputStream(bin);
+        DataInputStream din = new DataInputStream(bin);
         // Check the first 4 bytes to see if it's a ToureNPlaner Graphfile
         byte[] magic = new byte[4];
         din.readFully(magic);
         String magicString = new String(magic, Charset.forName("UTF-8"));
-        if(!magicString.equals("TPG\n")){
+        if (!magicString.equals("TPG\n")) {
             throw new IOException("The given stream does not contain a ToureNPlaner Graphfile");
         }
         // Check whether we got the same version
         int realVersion = din.readInt();
-        if(realVersion != version){
-            throw new IOException("Wrong file format version, expected "+version+ " got "+ realVersion);
+        if (realVersion != version) {
+            throw new IOException("Wrong file format version, expected " + version + " got " + realVersion);
         }
 
         int nodeCount = din.readInt();
-		int edgeCount = din.readInt();
+        int edgeCount = din.readInt();
 
-		GraphRep graphRep = new GraphRep(nodeCount, edgeCount);
+        GraphRep graphRep = new GraphRep(nodeCount, edgeCount);
 
 
-		log.info("Reading " + nodeCount + " nodes and " + edgeCount
-				+ " edges ...");
-		int lat, lon;
-		int height;
+        log.info("Reading " + nodeCount + " nodes and " + edgeCount + " edges ...");
+        int lat, lon;
+        int height;
         int rank;
-		for (int i = 0; i < nodeCount; i++) {
-			lat = din.readInt();
-			lon = din.readInt();
-			height = din.readInt();
+        // Read nodes
+        for (int i = 0; i < nodeCount; i++) {
+            lat = din.readInt();
+            lon = din.readInt();
+            height = din.readInt();
             rank = din.readInt();
-			graphRep.setNodeData(i, lat, lon, height);
-			graphRep.setNodeRank(i, rank);
-		}
+            graphRep.setNodeData(i, lat, lon, height);
+            graphRep.setNodeRank(i, rank);
+        }
 
-		// temporary values for edges
-		int src, trgt, dist, euclidianDist;
-		int shortcuttedEdge1, shortcuttedEdge2;
-		for (int i = 0; i < edgeCount; i++) {
-			src = din.readInt();
-			trgt = din.readInt();
-			dist = din.readInt();
+        // temporary values for edges
+        int src, trgt, dist, euclidianDist;
+        int shortcuttedEdge1, shortcuttedEdge2;
+        // Read edges
+        for (int i = 0; i < edgeCount; i++) {
+            src = din.readInt();
+            trgt = din.readInt();
+            dist = din.readInt();
             euclidianDist = din.readInt();
-			shortcuttedEdge1 = din.readInt();
-			shortcuttedEdge2 = din.readInt();
+            shortcuttedEdge1 = din.readInt();
+            shortcuttedEdge2 = din.readInt();
             graphRep.setEdgeData(i, src, trgt, dist, euclidianDist);
-			graphRep.setShortcutData(i, shortcuttedEdge1, shortcuttedEdge2);
-		}
-		in.close();
-		log.info("Start generating offsets");
-		graphRep.generateOffsets();
-		log.info("successfully created offset of InEdges");
+            graphRep.setShortcutData(i, shortcuttedEdge1, shortcuttedEdge2);
+        }
 
-		// choose the NNSearcher here
-		// DumbNN uses linear search and is slow.
-		// HashNN should be faster but needs more RAM
+        // Read mappingInToOut
+        final int[] mappingInToOut = new int[edgeCount];
+        for (int i = 0; i < edgeCount; i++) {
+            mappingInToOut[i] = din.readInt();
+        }
+        graphRep.setMappingInToOut(mappingInToOut);
+
+        // Read offsetIn
+        final int[] offsetIn = new int[nodeCount + 1];
+        for (int i = 0; i < nodeCount + 1; i++) {
+            offsetIn[i] = din.readInt();
+        }
+        graphRep.setOffsetIn(offsetIn);
+
+        // Read offsetOut
+        final int[] offsetOut = new int[nodeCount + 1];
+        for (int i = 0; i < nodeCount + 1; i++) {
+            offsetOut[i] = din.readInt();
+        }
+        graphRep.setOffsetOut(offsetOut);
+
+        in.close();
+        log.info("successfully created offset of InEdges");
+        // choose the NNSearcher here
+        // DumbNN uses linear search and is slow.
+        // HashNN should be faster but needs more RAM
         log.info("Start creating NNSearcher");
-		graphRep.searcher = new HashNN(graphRep);
-		System.gc();
-		log.info("Graph loaded");
-		return graphRep;
-	}
+        graphRep.searcher = new HashNN(graphRep);
+        System.gc();
+        log.info("Graph loaded");
+        return graphRep;
+    }
 }
--- src/graphrep/GraphRepBinaryWriter.java
+++ src/graphrep/GraphRepBinaryWriter.java
@@ -7,10 +7,11 @@ import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.util.logging.Logger;
 
-public class GraphRepBinaryWriter implements GraphRepWriter{
+public class GraphRepBinaryWriter implements GraphRepWriter {
     private static Logger log = Logger.getLogger("tourenplaner");
 
-    private static final int version = 1;
+    private static final int version = 2;
+
     @Override
     public void writeGraphRep(OutputStream out, GraphRep graphRep) throws IOException {
         // 8388608 Bytes = 8 MB
@@ -20,15 +21,15 @@ public class GraphRepBinaryWriter implements GraphRepWriter{
         dout.write("TPG\n".getBytes(Charset.forName("UTF-8")));
         // Write format version
         dout.writeInt(version);
-        
+
         // Write number of nodes and edges
         int numNodes = graphRep.getNodeCount();
         int numEdges = graphRep.getEdgeCount();
         dout.writeInt(numNodes);
         dout.writeInt(numEdges);
-        
+
         // Write nodes
-        for(int i = 0; i < numNodes; i++){
+        for (int i = 0; i < numNodes; i++) {
             dout.writeInt(graphRep.getNodeLat(i));
             dout.writeInt(graphRep.getNodeLon(i));
             dout.writeInt(graphRep.getNodeHeight(i));
@@ -36,7 +37,7 @@ public class GraphRepBinaryWriter implements GraphRepWriter{
         }
 
         // Write edges
-        for(int i = 0; i < numEdges; i++){
+        for (int i = 0; i < numEdges; i++) {
             dout.writeInt(graphRep.getSource(i));
             dout.writeInt(graphRep.getTarget(i));
             dout.writeInt(graphRep.getDist(i));
@@ -44,6 +45,28 @@ public class GraphRepBinaryWriter implements GraphRepWriter{
             dout.writeInt(graphRep.getFirstShortcuttedEdge(i));
             dout.writeInt(graphRep.getSecondShortcuttedEdge(i));
         }
+
+        // Write mapping
+        int[] mappingInToOut = graphRep.getMappingInToOut();
+
+        for (int i = 0; i < mappingInToOut.length; i++) {
+            dout.writeInt(mappingInToOut[i]);
+        }
+
+        // Write offsetIn
+        int[] offsetIn = graphRep.getOffsetIn();
+
+        for (int i = 0; i < offsetIn.length; i++) {
+            dout.writeInt(offsetIn[i]);
+        }
+
+        // Write offsetOut
+        int[] offsetOut = graphRep.getOffsetOut();
+
+        for (int i = 0; i < offsetOut.length; i++) {
+            dout.writeInt(offsetOut[i]);
+        }
+
         bout.flush();
         dout.flush();
         dout.close();
