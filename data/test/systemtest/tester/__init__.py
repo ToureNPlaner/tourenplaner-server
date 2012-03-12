2db26eff17ea5bcfed114999c3b9db3e2dd9a268
Niklas Schnelle <niklas@komani.de>
2012-03-12 14:31:58 +0100

    More tests better structure

--- data/test/systemtest/tester/__init__.py
+++ data/test/systemtest/tester/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import httplib2
 import json
 import random
@@ -12,10 +11,9 @@ class Requester:
    def doRequest(self, url, method, request):
       resp, content = self.http.request(uri=url, method=method, body=json.dumps(request), headers=self.headers)
       return resp, json.loads(str(content, "UTF-8"))
-   def setCredentials(self, name, password):      
+   def setCredentials(self, name, password):
       self.headers['authorization'] = 'Basic ' + base64.b64encode(("%s:%s" % (name, password)).encode('utf-8')).strip().decode('utf-8')
-      
-      
+
 class User:
    def __init__(self, email, password, firstName, lastName, address, admin, verified, indb):
       self.email = email
@@ -26,11 +24,11 @@ class User:
       self.admin = admin
       self.verified = verified
       self.indb = indb
-   
+
    def __str__(self):
       string = 'email: '+self.email+' pw: '+self.password+' fN: '+self.firstName+' lN: '+self.lastName
       return string+' addr: '+self.address+' (admin, verified, indb) ('+str(self.admin)+','+str(self.verified)+','+str(self.indb)+')'
-   
+
 class Test:
    def __init__(self, user):
       self.url = ''
@@ -39,25 +37,24 @@ class Test:
       self.request = ''
       self.resp = None
       self.content = None
-            
+
    def doTest(self, requester, baseurl):
       requester.setCredentials(self.user.email, self.user.password)
       self.resp, self.content = requester.doRequest(baseurl+self.url, self.method, self.request)
       return self.verifyResponse(self.resp, self.content)
-   
+
    def verifyResponse(self, resp, content):
       pass
 
-         
 class Tester:
    def __init__(self, baseurl ,requester, tests):
       self.requester = requester
       self.tests = tests
-      self.baseurl = baseurl   
-   
+      self.baseurl = baseurl
+
    def runTests(self):
       for test in self.tests:
-         result = test.doTest(self.requester, self.baseurl) 
+         result = test.doTest(self.requester, self.baseurl)
          if not result:
             print(test.__class__.__name__+' User: '+str(test.user.email)+' ----- FAILED')
             print(str(test.resp))
--- data/test/systemtest/tests.py
+++ data/test/systemtest/tests.py
@@ -1,27 +1,136 @@
 #!/usr/bin/env python3
 import httplib2
 import base64
+import urllib.parse
 from tester import Test
 from tester import User
 
+testClasses = []
 
+def TestClass(clazz):
+   testClasses.append(clazz)
+
+
+@TestClass
 class SPTest(Test):
    def __init__(self, user):
       super().__init__(user)
       self.url = '/algsp'
       # Stuttgart to Hamburg
       self.request = {'points':[{'lt': 487786110, 'ln' : 91794440}, {'lt': 535652780, 'ln': 100013890}]}
-   
+
    def verifyResponse(self, resp, content):
       # The distance should be 640 km < distance < 670 km
-      status = resp['status']      
+      status = resp['status']
       if not self.user.indb:
          return status == '401' and content['errorid'] == 'EAUTH'
       elif not self.user.verified:
          return status == '403' and content['errorid'] == 'ENOTVERIFIED'
       else:
          return resp['status'] == '200' and content['misc']['distance'] > 640000 and content['misc']['distance'] < 670000
-      
+
+@TestClass
+class AuthTest(Test):
+
+   def __init__(self, user):
+      super().__init__(user)
+      self.url = '/authuser'
+      self.method = 'GET'
+
+   def verifyResponse(self, resp, content):
+      status = resp['status']
+      if not self.user.indb:
+         return status == '401' and content['errorid'] == 'EAUTH'
+      elif not self.user.verified:
+         return status == '403' and content['errorid'] == 'ENOTVERIFIED'
+      else:
+         return resp['status'] == '200' and content['email'] == self.user.email and content['firstname'] == self.user.firstName and content['lastname'] == self.user.lastName
+
+
+@TestClass
+class ListUserTestNormal(Test):
+
+   def __init__(self, user):
+      super().__init__(user)
+      self.limit = 4
+      self. offset = 1
+      self.url = '/listusers?'+urllib.parse.urlencode({'limit':self.limit, 'offset': self.offset})
+      self.method = 'GET'
+
+   def verifyResponse(self, resp, content):
+      status = resp['status']
+      if not self.user.indb:
+         return status == '401' and content['errorid'] == 'EAUTH'
+      elif not self.user.verified:
+         return status == '403' and content['errorid'] == 'ENOTVERIFIED'
+      elif not self.user.admin:
+         return status == '403' and content['errorid'] == 'ENOTADMIN'
+      else:
+         # zero'th user is admin with userid==1
+         correct = status == '200' and len(content['users']) == self.limit
+         correct = correct and content['users'][0]['email'] == 'userinactivenotadmin@teufel.de'
+         correct = correct and content['users'][0]['status'] == 'needs_verification' and content['users'][1]['email'] == 'userinactiveadmin@teufel.de'
+         return correct
+
+@TestClass
+class ListUserTestWithoutLimit(Test):
+
+   def __init__(self, user):
+      super().__init__(user)
+      self. offset = 1
+      self.url = '/listusers?'+urllib.parse.urlencode({'offset': self.offset})
+      self.method = 'GET'
+
+   def verifyResponse(self, resp, content):
+      status = resp['status']
+      if not self.user.indb:
+         return status == '401' and content['errorid'] == 'EAUTH'
+      elif not self.user.verified:
+         return status == '403' and content['errorid'] == 'ENOTVERIFIED'
+      elif not self.user.admin:
+         return status == '403' and content['errorid'] == 'ENOTADMIN'
+      else:
+         return status == '400' and content['errorid'] == 'ELIMIT'
+
+@TestClass
+class ListUserTestWithoutOffset(Test):
+
+   def __init__(self, user):
+      super().__init__(user)
+      self.limit = 4
+      self.url = '/listusers?'+urllib.parse.urlencode({'limit' :self.limit})
+      self.method = 'GET'
+
+   def verifyResponse(self, resp, content):
+      status = resp['status']
+      if not self.user.indb:
+         return status == '401' and content['errorid'] == 'EAUTH'
+      elif not self.user.verified:
+         return status == '403' and content['errorid'] == 'ENOTVERIFIED'
+      elif not self.user.admin:
+         return status == '403' and content['errorid'] == 'ENOTADMIN'
+      else:
+         return status == '400' and content['errorid'] == 'EOFFSET'
+
+@TestClass
+class ListUserTestWithoutParams(Test):
+
+   def __init__(self, user):
+      super().__init__(user)
+      self. offset = 1
+      self.url = '/listusers'
+      self.method = 'GET'
+
+   def verifyResponse(self, resp, content):
+      status = resp['status']
+      if not self.user.indb:
+         return status == '401' and content['errorid'] == 'EAUTH'
+      elif not self.user.verified:
+         return status == '403' and content['errorid'] == 'ENOTVERIFIED'
+      elif not self.user.admin:
+         return status == '403' and content['errorid'] == 'ENOTADMIN'
+      else:
+         return status == '400' and (content['errorid'] == 'ELIMIT' or content['errorid'] == 'EOFFSET')
 
 
 users = [
@@ -30,8 +139,8 @@ users = [
          User('userinactiveadmin@teufel.de', 'only4testing', 'InactiveAdmin', 'Teufel','', admin=True, verified=False, indb=True),
          User('useractivenotadmin@teufel.de', 'only4testing', 'ActiveNotAdmin', 'Teufel','', admin=False, verified=True, indb=True),
          User('useractiveadmin@teufel.de', 'only4testing', 'ActiveAdmin', 'Teufel','', admin=True, verified=True, indb=True)]
-         
-testClasses = [SPTest]
+
+
 allTests = [testClass(user) for testClass in testClasses for user in users]
 
-         
+
