b2222caf58333e19f495738ea7e44e831c59cca7
Niklas Schnelle <niklas@komani.de>
2012-04-01 17:28:20 +0200

    Add travelTime to TravelingSalesman

--- src/de/tourenplaner/algorithms/TravelingSalesman.java
+++ src/de/tourenplaner/algorithms/TravelingSalesman.java
@@ -5,17 +5,19 @@ import com.carrotsearch.hppc.IntOpenHashSet;
 import de.tourenplaner.computecore.ComputeRequest;
 import de.tourenplaner.computecore.RequestPoint;
 import de.tourenplaner.computecore.RequestPoints;
+import de.tourenplaner.computecore.Way;
 import de.tourenplaner.graphrep.GraphRep;
 import de.tourenplaner.utils.StaticMath;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
- *  @author Christoph Haag, Sascha Meusel, Niklas Schnelle, Peter Vollmer
- *
- * Used to create instances of TravelingSalesman algorithm
+ * @author Christoph Haag, Sascha Meusel, Niklas Schnelle, Peter Vollmer
+ *         <p/>
+ *         Used to create instances of TravelingSalesman algorithm
  */
 public class TravelingSalesman extends GraphAlgorithm {
     private static final int SMALL = 12;
@@ -56,22 +58,22 @@ public class TravelingSalesman extends GraphAlgorithm {
 
     @Override
     public void compute(ComputeRequest req) throws ComputeException {
-       
+
         // Map points to ids
         RequestPoints points = req.getPoints();
-        if(points.size() < 2){
+        if (points.size() < 2) {
             throw new ComputeException("Not enough points, need at least 2");
         }
         int[][] distmat;
 
-      try {
+        try {
             // Map our requested points to ids
             points.setIdsFromGraph(graph);
 
             // Looks cheap but computes the n^2 matrix of distances for the given points
             distmat = computeDistMatrix(points);
             int[] currTour;
-            if (points.size() < SMALL){
+            if (points.size() < SMALL) {
                 // It's small use the Giant Hammer Method (TM)
                 currTour = exactSolve(distmat);
             } else {
@@ -83,53 +85,62 @@ public class TravelingSalesman extends GraphAlgorithm {
             List<RequestPoint> pointStore = new ArrayList<RequestPoint>(points.size());
             List<RequestPoint> requestPointList = points.getStore();
             // Rearrange the point store to the found bestTour
-          for (int currTourEntry : currTour) {
-              pointStore.add(requestPointList.get(currTourEntry));
-          }
+            for (int currTourEntry : currTour) {
+                pointStore.add(requestPointList.get(currTourEntry));
+            }
             req.getPoints().setStore(pointStore);
             // Now build real paths
-            int distance = chdijks.shortestPath(points, req.getResultWays(), true);
-            HashMap<String, Object> misc = new HashMap<String, Object>(1);
+            List<Way> resultWays = req.getResultWays();
+            int distance = chdijks.shortestPath(points, resultWays, true);
+            // Calculate total time
+            int numWays = resultWays.size();
+            double totalTime = 0;
+            for (int i = 0; i < numWays; i++) {
+                totalTime += resultWays.get(i).getTravelTime();
+            }
+
+            Map<String, Object> misc = new HashMap<String, Object>(1);
             misc.put("distance", distance);
+            misc.put("time", totalTime);
             req.setMisc(misc);
 
         } catch (IllegalAccessException e) {
             throw new ComputeException("Illegal Access: " + e.getMessage());
         }
     }
-    
-    private final int[] exactSolve(int[][] distmat){
+
+    private final int[] exactSolve(int[][] distmat) {
         int[] bestTour = null;
         int[] currTour = new int[distmat.length];
         // Begin with tour in original/identity permutation
-        for(int i = 0; i< currTour.length; i++){
+        for (int i = 0; i < currTour.length; i++) {
             currTour[i] = i;
         }
         int bestLength = Integer.MAX_VALUE;
         int currLength;
         do {
             currLength = calcTourLength(distmat, currTour);
-            if(currLength < bestLength){
+            if (currLength < bestLength) {
                 bestTour = currTour.clone();
                 bestLength = currLength;
             }
-        } while(StaticMath.nextPerm(currTour));
+        } while (StaticMath.nextPerm(currTour));
 
-        return  bestTour;
+        return bestTour;
     }
 
-    private final int[] twoOpt(int[][] distmat, int[] currTour){
+    private final int[] twoOpt(int[][] distmat, int[] currTour) {
         int[] shiftedTour = currTour.clone();
 
 
         int[] bestTour = shiftedTour.clone();
         int bestLength = calcTourLength(distmat, bestTour);
-        for(int i = 0; i < shiftedTour.length; i++){
-            for(int j = 1;j< shiftedTour.length-1;j++){
+        for (int i = 0; i < shiftedTour.length; i++) {
+            for (int j = 1; j < shiftedTour.length - 1; j++) {
                 int[] neighborTour = shiftedTour.clone();
-                StaticMath.reverse(neighborTour, 0,j+1);
+                StaticMath.reverse(neighborTour, 0, j + 1);
                 int length = calcTourLength(distmat, neighborTour);
-                if(length < bestLength){
+                if (length < bestLength) {
                     bestTour = neighborTour.clone();
                 }
 
@@ -143,27 +154,26 @@ public class TravelingSalesman extends GraphAlgorithm {
             // shift  array to the left
             int first = shiftedTour[0];
             System.arraycopy(shiftedTour, 1, shiftedTour, 0, shiftedTour.length - 1);
-            shiftedTour[shiftedTour.length-1] = first;
+            shiftedTour[shiftedTour.length - 1] = first;
         }
 
         return bestTour;
     }
-    
-    private final int calcTourLength(int[][] distmat, int[] tourPerm){
+
+    private final int calcTourLength(int[][] distmat, int[] tourPerm) {
         int length = 0;
-        for(int i = 0; i < tourPerm.length - 1; i++){
-            length += distmat[tourPerm[i]][tourPerm[i+1]];
+        for (int i = 0; i < tourPerm.length - 1; i++) {
+            length += distmat[tourPerm[i]][tourPerm[i + 1]];
         }
         // We have to make sure beforehand that there is an element
         // because this will be called A LOT save that check
-        length +=  distmat[tourPerm[tourPerm.length - 1]][tourPerm[0]];
+        length += distmat[tourPerm[tourPerm.length - 1]][tourPerm[0]];
         return length;
     }
 
     /**
      * Computes a tour (permutation) with the Way ordered by the nearest neighbor
      * heuristic starting at the first point successively adding the nearest unvisited point
-     *
      *
      * @param distmat the n^2 matrix of distance values for the given points
      * @return
@@ -175,7 +185,7 @@ public class TravelingSalesman extends GraphAlgorithm {
         int minValue;
         int minIndex;
         // add the initial point
-        tour[0]=0;
+        tour[0] = 0;
         for (int nextIndex = 1; nextIndex < tour.length; nextIndex++) {
             visited.add(currIndex);
 
@@ -188,7 +198,7 @@ public class TravelingSalesman extends GraphAlgorithm {
                 }
             }
             currIndex = minIndex;
-            tour[nextIndex]= currIndex;
+            tour[nextIndex] = currIndex;
         }
         return tour;
     }
