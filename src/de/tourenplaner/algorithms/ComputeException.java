b868db5558ada5740b00d3840d1a5c99ed6d41f2
Sascha Meusel <meuselsa@studi.informatik.uni-stuttgart.de>
2012-04-13 12:47:06 +0200

    Added JavaDoc to ComputeException

--- src/de/tourenplaner/algorithms/ComputeException.java
+++ src/de/tourenplaner/algorithms/ComputeException.java
@@ -4,6 +4,11 @@
 package de.tourenplaner.algorithms;
 
 /**
+ * An algorithm should throw the ComputeException if expected errors
+ * occur during normal server activity and normal server state.
+ * Do not use this exception if the error occurs because of server problems
+ * or faulty algorithm code.
+ *
  * @author Christoph Haag, Sascha Meusel, Niklas Schnelle, Peter Vollmer
  * 
  */
