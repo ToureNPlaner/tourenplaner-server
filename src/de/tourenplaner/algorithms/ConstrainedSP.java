669a32f76f727f04f7665308be6e6ebebce9a13d
Niklas Schnelle <niklas@komani.de>
2012-04-16 18:20:22 +0200

    Add Apache 2.0 Licence information

--- src/de/tourenplaner/algorithms/ConstrainedSP.java
+++ src/de/tourenplaner/algorithms/ConstrainedSP.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 ToureNPlaner
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package de.tourenplaner.algorithms;
 
 import de.tourenplaner.computecore.ComputeRequest;
@@ -128,7 +144,7 @@ public class ConstrainedSP extends GraphAlgorithm {
                 log.finer("There is no path from src: " + srcId + " to trgt: " + trgtId + "with constraint" +
                         maxAltitudeDifference
                 );
-                misc.put("message", "Not feasible, but found path with smallest altitude difference = "+ altitudeDiff+" m");
+                misc.put("message", "Not feasible, but found path with smallest altitude difference = " + altitudeDiff + " m");
                 ds.returnDistArray(false);
                 ds.returnHeap();
                 ds.returnPrevArray();
