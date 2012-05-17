f46a41d81515f4a0ec1ad8eb53bf19513ef67cda
Niklas Schnelle <niklas@komani.de>
2012-05-17 20:01:31 +0200

    Missed writing misc field and closing generator

--- src/de/tourenplaner/computecore/ComputeRequest.java
+++ src/de/tourenplaner/computecore/ComputeRequest.java
@@ -186,5 +186,9 @@ public class ComputeRequest {
             }
             gen.writeEndArray();
         }
+        gen.writeEndArray();
+        gen.writeObjectField("misc", this.misc);
+        gen.writeEndObject();
+        gen.close();
     }
 }
