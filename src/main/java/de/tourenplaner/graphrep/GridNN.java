192bb211f04df713dbc9512d5034fd4ecd4b2347
Niklas Schnelle <niklas@komani.de>
2012-09-29 23:02:22 +0200

    REVERT LATER For testing don't fall back to DumbNN

--- src/main/java/de/tourenplaner/graphrep/GridNN.java
+++ src/main/java/de/tourenplaner/graphrep/GridNN.java
@@ -165,7 +165,7 @@ public class GridNN implements NNSearcher {
         // If every list was null fallback
         if (minDist == Long.MAX_VALUE){
             log.warning("Fell back to dumbNN for "+lat+","+lon);
-            minNodeId = fallback.getIDForCoordinates(lat, lon);
+            minNodeId = 0;//fallback.getIDForCoordinates(lat, lon);
         }
         return minNodeId;
 
