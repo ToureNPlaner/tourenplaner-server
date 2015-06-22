ee9feeac9a350a55ed6f9181798a84b378ccc73e
Niklas Schnelle <niklas@komani.de>
2015-06-22 11:32:16 +0200

    Don't supply draw data outside bbox at all

--- src/main/java/de/tourenplaner/algorithms/bbbundle/EdgeUnpacker.java
+++ src/main/java/de/tourenplaner/algorithms/bbbundle/EdgeUnpacker.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 
 /**
  * Manages unpacking of edges and tracking which have alredy been rembebered for drawing
- *
+ * <p>
  * Created by niklas on 15.06.15.
  */
 public class EdgeUnpacker {
@@ -25,22 +25,14 @@ public class EdgeUnpacker {
         int unpackedIndex = (edgesToDraw.size() - 1);
         unpackedMap[edgeId] = unpackedIndex;
         int reverseEdgeId = graph.getReverseEdgeId(edgeId);
-        if(reverseEdgeId > 0) {
+        if (reverseEdgeId > 0) {
             unpackedMap[reverseEdgeId] = unpackedIndex;
         }
         unpackedIndices.add(unpackedIndex);
     }
 
     public void unpack(BoundingBox bbox, int edgeId, IntArrayList unpackedIndices, IntArrayList edgesToDraw, double minLen, double maxLen, double maxRatio) {
-        unpackRecursive(bbox, edgeId, unpackedIndices, edgesToDraw, minLen, maxLen, maxRatio);
-        // We need at least one unpacking to get coordinates for the nodes
-        if (unpackedIndices.size() < 1) {
-            addEdge(edgeId, unpackedIndices, edgesToDraw);
-        }
-    }
-
-    private void unpackRecursive(BoundingBox bbox, int edgeId, IntArrayList unpackedIndices, IntArrayList edgesToDraw, double minLen, double maxLen, double maxRatio) {
-        if (unpackedMap[edgeId] >= 0 ){
+        if (unpackedMap[edgeId] >= 0) {
             unpackedIndices.add(unpackedMap[edgeId]);
             return;
         }
@@ -53,40 +45,43 @@ public class EdgeUnpacker {
             return;
         }
 
-        int skipB = graph.getSecondShortcuttedEdge(edgeId);
-
         int srcId = graph.getSource(edgeId);
-        double x1 = graph.getXPos(srcId);
-        double y1 = graph.getYPos(srcId);
+        int x1 = graph.getXPos(srcId);
+        int y1 = graph.getYPos(srcId);
         int trgtId = graph.getTarget(edgeId);
-        double x3 = graph.getXPos(trgtId);
-        double y3 = graph.getYPos(trgtId);
-        if(!bbox.contains((int) x1, (int) y1) && !bbox.contains((int)x3, (int)y3)){
+        int x3 = graph.getXPos(trgtId);
+        int y3 = graph.getYPos(trgtId);
+        if (!bbox.contains(x1, y1) && !bbox.contains(x3, y3)) {
             return;
         }
 
         /*
-		*              |x1 y1 1|
-		* A = abs(1/2* |x2 y2 1|)= 1/2*Baseline*Height
-		*              |x3 y3 1|
-		*   = 1/2*((x1*y2+y1*x3+x2*y3) - (y2*x3 + y1*x2 + x1*y3))
-		* Height = 2*A/Baseline
-		* ratio = Height/Baseline
-		* */
+        *              |x1 y1 1|
+        * A = abs(1/2* |x2 y2 1|)= 1/2*Baseline*Height
+        *              |x3 y3 1|
+        *   = 1/2*((x1*y2+y1*x3+x2*y3) - (y2*x3 + y1*x2 + x1*y3))
+        * Height = 2*A/Baseline
+        * ratio = Height/Baseline
+        * */
         if (edgeLen <= maxLen) {
             int middle = graph.getTarget(skipA);
-            double x2 = graph.getXPos(middle);
-            double y2 = graph.getYPos(middle);
-            double A = Math.abs(0.5 * ((x1 * y2 + y1 * x3 + x2 * y3) - (y2 * x3 + y1 * x2 + x1 * y3)));
+            double x2d = graph.getXPos(middle);
+            double y2d = graph.getYPos(middle);
+            double A = Math.abs(0.5 * (
+                    ((double) x1 * y2d + (double) y1 * (double) x3 + x2d * (double) y3)
+                    - (y2d * (double) x3 + (double) y1 * x2d + (double) x1 * (double) y3)
+            ));
             double ratio = 2.0 * A / (edgeLen * edgeLen);
+
             if (ratio <= maxRatio) {
                 addEdge(edgeId, unpackedIndices, edgesToDraw);
                 return;
             }
         }
 
-        unpackRecursive(bbox, skipA, unpackedIndices, edgesToDraw, minLen, maxLen, maxRatio);
-        unpackRecursive(bbox, skipB, unpackedIndices, edgesToDraw, minLen, maxLen, maxRatio);
+        unpack(bbox, skipA, unpackedIndices, edgesToDraw, minLen, maxLen, maxRatio);
+        int skipB = graph.getSecondShortcuttedEdge(edgeId);
+        unpack(bbox, skipB, unpackedIndices, edgesToDraw, minLen, maxLen, maxRatio);
     }
 
     public void reset() {
